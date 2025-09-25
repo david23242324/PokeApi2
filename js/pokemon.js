caa0480cea8e73c4d20677b4c30d85741678c997
david23242324 <davidshelby2324@gmail.com>
2025-09-25 21:27:38 +0000

    Cambios en js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,9 +1,29 @@
-async function pokemon() {
+async function pokemon(parametro) {
             var root = document.getElementById("root");
-    const res = await fetch (`https://pokeapi.co/api/v2/pokemon/pikachu`);
+    const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${parametro}`);
     const data = await res.json();
 
-    console.log(data.forms[0].name);
+
+    let tipoPoke = "";
+    for (let i = 0; i < data.types.length; i++) {
+        tipoPoke += `<span>${data.types[i].type.name}</span>`;
+    }
+
+    root.innerHTML = `
+    
+    <section class="c-detalle>
+    <image src="https://raw.githubusercontent.com/PokeAPI"
+    <p>${data.name}</p>
+    <p>${data.id}</p>
+    <p>Altura: ${data.height / 10} = Peso: ${data.weight / 10} kg</p>
+    <p>Hp: ${data.stats[0].base_stat}</p>
+    <p>Velocidad: ${data.stats[5].base_stat}</p>
+    <p>Ataque: ${data.stats[i].base_stat} Defensa: ${data.base_stat[2].base_stat}</p>
+    <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.base_stat}</p>
+    
+    </section>
+    
+    `;
 }
 
-pokemon()
+pokemon(5)
