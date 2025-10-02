8b8332a7f0d57e8e57b855a329cbdba71521ce1b
david23242324 <davidshelby2324@gmail.com>
2025-10-02 01:06:46 +0000

    Cambios

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,29 +1,30 @@
 async function pokemon(parametro) {
-            var root = document.getElementById("root");
-    const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${parametro}`);
+    const root = document.getElementById("root");
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${parametro}`);
     const data = await res.json();
 
-
+    // Obtener tipos
     let tipoPoke = "";
     for (let i = 0; i < data.types.length; i++) {
-        tipoPoke += `<span>${data.types[i].type.name}</span>`;
+        tipoPoke += `<span>${data.types[i].type.name}</span> `;
     }
 
+    // Mostrar datos
     root.innerHTML = `
-    
-    <section class="c-detalle>
-    <image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites">Imagen</image>
-    <p>${data.name}</p>
-    <p>${data.id}</p>
-    <p>Altura: ${data.height / 10} = Peso: ${data.weight / 10} kg</p>
-    <p>Hp: ${data.stats[0].base_stat}</p>
-    <p>Velocidad: ${data.stats[5].base_stat}</p>
-    <p>Ataque: ${data.stats[i].base_stat} Defensa: ${data.base_stat[2].base_stat}</p>
-    <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.base_stat}</p>
-    
+    <section class="c-detalle">
+        <img src="${data.sprites.front_default}" alt="${data.name}">
+        <p>Nombre: ${data.name}</p>
+        <p>ID: ${data.id}</p>
+        <p>Altura: ${data.height / 10} m | Peso: ${data.weight / 10} kg</p>
+        <p>Tipos: ${tipoPoke}</p>
+        <p>HP: ${data.stats[0].base_stat}</p>
+        <p>Velocidad: ${data.stats[5].base_stat}</p>
+        <p>Ataque: ${data.stats[1].base_stat} | Defensa: ${data.stats[2].base_stat}</p>
+        <p>Ataque Especial: ${data.stats[3].base_stat} | Defensa Especial: ${data.stats[4].base_stat}</p>
     </section>
-    
     `;
 }
 
-pokemon(5)
+pokemon(5);
+pokemon(55);
+
